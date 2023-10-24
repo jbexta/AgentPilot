d4685437b9976258fe2bf99b40f0336057eb8ee2
jbexta <jake.c.beck@gmail.com>
2023-10-24 23:48:06 +0100

    fix api env var

--- agentpilot/utils/api.py
+++ agentpilot/utils/api.py
@@ -1,10 +1,6 @@
 import os
-
 import openai
-# import agentpilot.operations.src.
-
 from utils import sql
-import litellm
 
 apis = {}
 llm_apis = {}
@@ -15,18 +11,9 @@ def load_api_keys():
     api_table = sql.get_results("SELECT `name`, `client_key`, `priv_key` FROM apis")
     apis = {api[0].lower(): {'client_key': api[1], 'priv_key': api[2]} for api in api_table}
 
-    dev_key = os.environ.get("OPENAI_API_KEY", None)
-    if dev_key:
-        apis['openai']['priv_key'] = dev_key
+    if apis['openai']['priv_key'] == '$OPENAI_API_KEY':
+        apis['openai']['priv_key'] = os.environ.get("OPENAI_API_KEY", '')
     openai.api_key = apis['openai']['priv_key']
 
 
-# def set_llm_api_keys():
-#     for api_name, api_config in apis.items():
-#         if api_name == 'openai':
-#             litellm.openai_key openai.api_key = api_config['priv_key']
-#             litellm.openai_key = api_config['priv_key']
-
-
 load_api_keys()
-# set_llm_api_keys()
