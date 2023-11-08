aa5d0241e776a23f2b9fea87b6311c15f8973554
jbexta <jake.c.beck@gmail.com>
2023-11-08 14:01:41 +0000

    fix icons

--- agentpilot/utils/filesystem.py
+++ agentpilot/utils/filesystem.py
@@ -7,9 +7,15 @@ def get_application_path():
         print("sys.executable:", sys.executable)
         return os.path.dirname(os.path.abspath(sys.executable))
     elif sys.platform == 'linux':
+        print(os.environ.get('APPIMAGE'))
+        # print other possible executable paths
+        print("sys.executable:", sys.executable)
+        print("dirname of os.path.abspath(sys.executable):", os.path.dirname(os.path.abspath(sys.executable)))
+        print("dirname of os.path.abspath(__file__):", os.path.dirname(os.path.abspath(__file__)))
+        print("dirname of os.path.abspath(os.path.dirname(__file__)):", os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
         app_image_var = os.environ.get('APPIMAGE')
         if not app_image_var:
-            raise NotImplementedError()
+            app_image_var = os.path.abspath(sys.executable)
         return os.path.dirname(app_image_var)
     elif sys.platform == 'darwin':  # Mac OS
         print("sys.executable:", sys.executable)
